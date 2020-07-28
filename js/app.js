522841a449bc9764ce6c82713bb118d2a9098526
kiskee <53805976+kiskee@users.noreply.github.com>
2020-07-28 17:27:28 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -133,7 +133,7 @@ function columnasDulces(index){
 }
 
 
-// 3 valida si se eliminan dulces
+// 3 valida si se eliminan dulces en columna
 
 function validacionColumna(){
   for (var n = 0; n < 7; n++){
@@ -144,7 +144,166 @@ function validacionColumna(){
     var comparaValor = columnaDulce.eq(0);
     var gap = false;
     for (var i = 1; i < columnaDulce.length; i++){
+      var sscompara = comparaValor.attr('src');
+      var ssdulce = columnaDulce.eq(i).attr('src');
 
+      if (sscompara != ssdulce){
+        if (dulcePosicion.length >= 3){
+          gap = true;
+        }else {
+          dulcePosicion = [];
+        }
+        contando = 0;
+      }else {
+        if (contando == 0){
+          if (!gap){
+            dulcePosicion.push(i-1);
+          }else {
+            dulceAdicionalPosicion.push(i-1);
+          }
+        }
+        if (!gap){
+          dulcePosicion.push(i);
+        }else {
+          dulceAdicionalPosicion.push(i);
+        }
+        contando += 1;
+      }
+        comparaValor = columnaDulce.eq(i);
+    }
+    if (dulceAdicionalPosicion.length > 2 ){
+      dulcePosicion = $.merge(dulcePosicion, dulceAdicionalPosicion);
+    }
+    if (dulcePosicion.length <= 2){
+      dulcePosicion = [];
+    }
+    cuentaDulce = dulcePosicion.length;
+    if (cuentaDulce >= 3){
+      eliminaColumnaDulce(dulcePosicion, columnaDulce);
+      fijaPuntaje(cuentaDulce);
     }
   }
 }
+ function eliminaColumnaDulce(dulcePosicion, columnaDulce){
+   for(var i = 0;i < dulcePosicion.length;i++){
+     columnaDulce.eq(dulcePosicion[i].addClass('delete'))
+   }
+ }
+
+// valida si se eliminan dulces en filas
+
+function validafila(){
+  for (var n = 0; n < 6; n++){
+    var contando = 0;
+    var dulcePosicion =[];
+    var dulceAdicionalPosicion = [];
+    var filaDulce = filaDulces(n);
+    var comparaValor = filaDulce[0];
+    var gap = false;
+    for (var i = 1; i < filaDulce.length; i++){
+      var sscompara = comparaValor.attr('src');
+      var ssdulce = filaDulce[i].attr('src');
+
+      if (sscompara != ssdulce){
+        if (dulcePosicion.length >= 3){
+          gap = true;
+        }else {
+          dulcePosicion = [];
+        }
+        contando = 0;
+      }else {
+        if (contando == 0){
+          if (!gap){
+            dulcePosicion.push(i-1);
+          }else {
+            dulceAdicionalPosicion.push(i-1);
+          }
+        }
+        if (!gap){
+          dulcePosicion.push(i);
+        }else {
+          dulceAdicionalPosicion.push(i);
+        }
+        contando += 1;
+      }
+        comparaValor = filaDulce[i];
+    }
+    if (dulceAdicionalPosicion.length > 2 ){
+      dulcePosicion = $.merge(dulcePosicion, dulceAdicionalPosicion);
+    }
+    if (dulcePosicion.length <= 2){
+      dulcePosicion = [];
+    }
+    cuentaDulce = dulcePosicion.length;
+    if (cuentaDulce >= 3){
+      eliminafila(dulcePosicion, filaDulce);
+      fijaPuntaje(cuentaDulce);
+    }
+  }
+}
+ function eliminafila(dulcePosicion,filaDulce){
+   for(var i=0;i < dulcePosicion.length;i++){
+     filaDulce[dulcePosicion[i]].addClass('delete');
+   }
+ }
+
+
+ // puntiacion
+
+ function fijaPuntaje(cuentaDulce){
+   var puntaje = Number($('#score-text').text());
+   switch (cuentaDulce){
+    case 3:
+      puntaje += 25;
+      break;
+    case 4:
+      puntaje += 50;
+      break;
+    case 5:
+  		puntaje += 75;
+  		break;
+  	case 6:
+  		puntaje += 100;
+  		break;
+  	case 7:
+  		puntaje += 200;
+   }
+   $('#score-text').text(puntaje);
+ }
+
+
+ // dulces en pantalla
+
+ function  chekeatablero(){
+   llenatablero();
+ }
+
+ function llenatablero(){
+   var top = 6;
+   var columna = $('[class^="col-"]');
+
+   columna.each(function(){
+     var dulces = $(this).children().length;
+     var agrega = top - dulces;
+     for (var i = 0; i < agrega; i++){
+       var tipodulce = numeroRandom(1,5);
+       if (i === 0 && dulces < 1){
+         	$(this).append('<img src="image/' + tipodulce + '.png" class="element"></img>');
+       }else {
+         	$(this).find('img:eq(0)').before('<img src="image/' + tipodulce + '.png" class="element"></img>');
+       }
+     }
+   });
+   anadirEventoDulce();
+   validaciones();
+ }
+
+ // Si hay dulces que borrar
+ function validaciones() {
+ 	validacionColumna();
+ 	validafila();
+ 	// Si hay dulces que borrar
+ 	if ($('img.delete').length !== 0) {
+ 		eliminadulces();
+ 	}
+ }
